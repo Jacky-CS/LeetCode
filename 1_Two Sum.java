68b0fe065a0dcad5ba40d510bcbe2324a1ee68de
Jacky-CS <85839857+Jacky-CS@users.noreply.github.com>
2021-06-14 12:51:37 +0800

    Update 1_Two Sum.java

--- 1_Two Sum.java
+++ 1_Two Sum.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-//         Approach 1: Brute Force
-        for(int i = 0; i<nums.length; i++){
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i]+nums[j] == target){
-                    return new int[]{i,j};
-                }
-            }
-        }
-        return new int[2];
+/*         Approach 1: Brute Force*/
+//         for(int i = 0; i<nums.length; i++){
+//             for(int j = i+1; j<nums.length; j++){
+//                 if(nums[i]+nums[j] == target){
+//                     return new int[]{i,j};
+//                 }
+//             }
+//         }
+//         return new int[2];
         
-//         Approach 2: One-pass Hash Table
+/*        Approach 2: One-pass Hash Table*/
         Map<Integer, Integer> map = new HashMap();
-        
         for(int i = 0; i < nums.length; i++) {
             if(map.containsKey(target - nums[i]))
                 return new int[] {i, map.get(target - nums[i])};
             map.put(nums[i], i);
         }
-        
         return new int[2];
         
         
